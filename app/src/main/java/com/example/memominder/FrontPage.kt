7679d38e95c1bda35dc9ecfecdb869c8cb06b2c9
BorjaFornasC <bfornashiva@gmail.com>
2023-12-05 23:15:54 +0100

    SplashScreen y portada cambiada

--- app/src/main/java/com/example/memominder/FrontPage.kt
+++ app/src/main/java/com/example/memominder/FrontPage.kt
@@ -3,6 +3,9 @@ package com.example.memominder
 import android.annotation.SuppressLint
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,16 +13,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,9 +30,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -55,188 +57,22 @@ fun frontPage(navController: NavHostController) {
     ) {
 
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Text(text = "Front Page")
+            welcome()
         }
-
-        ConsultaArticulo()
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ConsultaArticulo() {
-    val contexto = LocalContext.current
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        var fecha by remember { mutableStateOf("") }
-        var actividad by remember { mutableStateOf("") }
-        var mensaje by remember { mutableStateOf("") }
-        OutlinedTextField(
-            value = fecha,
-            onValueChange = { fecha = it },
-            label = {
-                Text("Fecha")
-            },
+fun welcome() {
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.logoblack),
+            contentDescription = "Logotipo Splash Screen",
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp),
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                .size(500.dp)
         )
-        OutlinedTextField(
-            value = actividad,
-            onValueChange = { actividad = it },
-            label = {
-                Text("Actividad")
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp),
-            singleLine = true
-        )
-        Button(
-            onClick = {
-                ConsultDate(
-                    date = fecha,
-                    response = {
-                        if (it!=null) {
-                            actividad = it.activities
-                            mensaje=""
-                        } else {
-                            mensaje = "No existe el código de producto ingresado"
-                            actividad=""
-                        }
-                    },
-                    context = contexto
-                )
-            },
-            modifier = Modifier.padding(10.dp)
-        ) {
-            Text(text = "Consultar por código")
-        }
-        Button(
-            onClick = {
-                ConsultDate(
-                    date = fecha,
-                    response = {
-                        if (it!=null) {
-                            var actividadesActual = it.activities.split("&&")
-                            if (actividadesActual.contains(actividad)) {
-                                mensaje = "Ya tienes puesta esta actividad este día"
-                            } else {
-                                var actividades = ""
-                                for (i in actividadesActual) {
-                                    actividades += i + "&&"
-                                }
-                                actividades += actividad
-                                Modify(
-                                    activities = DayActivities(fecha, actividades),
-                                    response = {
-                                        if (it)
-                                            mensaje="Los datos fueron modificados"
-                                        else
-                                            mensaje = "No existe el código de producto ingresado"
-                                    }, context = contexto
-                                )
-                            }
-                            actividad = ""
-                            mensaje=""
-                        } else {
-                            AltaFecha(
-                                date = fecha,
-                                activities = actividad,
-                                context = contexto,
-                                response = {
-                                    if (it) {
-                                        mensaje = "se cargaron los datos"
-                                        fecha=""
-                                        actividad=""
-                                    }
-                                    else
-                                        mensaje="problemas en la carga"
-                                }
-                            )
-                            mensaje = "No existe el código de producto ingresado"
-                            actividad=""
-                        }
-                    }, context = contexto
-                )
-            },
-            modifier = Modifier.padding(10.dp)
-        ) {
-            Text(text = "Agregar")
-        }
-        Button(
-            onClick = {
-                ConsultDate(
-                    date = fecha,
-                    response = {
-                        if (it!=null) {
-                            var actividadesActual = it.activities.split("&&")
-                            if (actividadesActual.size == 1) {
-                                Delete(
-                                    date = fecha,
-                                    response = {
-                                        if (it) {
-                                            mensaje = "Se eliminó el artículo"
-                                            fecha=""
-                                            actividad=""
-                                        } else
-                                            mensaje = "No existe el código de producto ingresado"
-                                    }, contexto
-                                )
-                            } else {
-                                var actividades = ""
-                                for (i in actividadesActual) {
-                                    if (i != actividad) {
-                                        if (i == actividadesActual[actividadesActual.size - 1]) {
-                                            actividades += i
-                                        } else {
-                                            actividades += i + "&&"
-                                        }
-                                    }
-                                }
-                                Modify(
-                                    activities = DayActivities(fecha, actividades),
-                                    response = {
-                                        if (it)
-                                            mensaje="Los datos fueron modificados"
-                                        else
-                                            mensaje = "No existe el código de producto ingresado"
-                                    }, context = contexto
-                                )
-                            }
-                            actividad = ""
-                            mensaje=""
-                        } else {
-                            mensaje = "No hay nada que borrar en esa fecha o no existe esa fecha"
-                        }
-                    }, context = contexto
-                )
-            },
-            modifier = Modifier.padding(10.dp)
-        ) {
-            Text(text = "Borrar")
-        }
-        Button(onClick = { ConsultDate(
-            date = fecha,
-            response = {
-                if (it!=null) {
-                    actividad = it.activities
-                    mensaje=""
-                } else {
-                    mensaje = "No existe el código de producto ingresado"
-                    actividad=""
-                }
-            },
-            context = contexto
-        )
-        }, modifier = Modifier.padding(10.dp)) {
-            Text(text = "Listar")
-        }
-        listDay(date = fecha, activities = actividad)
-        Text(text = "$mensaje")
     }
 }
 
--- app/src/main/java/com/example/memominder/MainActivity.kt
+++ app/src/main/java/com/example/memominder/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.memominder
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Face
@@ -17,18 +27,33 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.memominder.ui.theme.FontTittle
 import com.example.memominder.ui.theme.MemoMinderTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -47,16 +72,26 @@ class MainActivity : ComponentActivity() {
                             .padding(top = it.calculateTopPadding())
                     ) {
                         val navController = rememberNavController()
-                        NavHost(navController = navController, startDestination = "Portada") {
-                            composable("Portada") {frontPage(navController) }
-                            composable("Calendar") { Calendar(navController = navController) }
+                        NavHost(navController = navController, startDestination = DestinationScreen.SplashScreenDest.route) {
+                            composable(route = DestinationScreen.SplashScreenDest.route) {
+                                SplashScreen(navController = navController)
+                            }
+                            composable("Portada") { frontPage(navController) }
+                            composable("Calendar", enterTransition = {slideIntoContainer(
+                                towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                                animationSpec = tween(700)
+                            )}, exitTransition = {fadeOut(animationSpec = tween(500))}) { Calendar(navController = navController) }
                             composable("Day/{date}",
                                 arguments = listOf(navArgument("date")
                                 {type = NavType.StringType}))
                             {backStackEntry ->
                                 day(backStackEntry.arguments?.getString("date") ?: "", navController)
                             }
-                            composable("Diary") { diary(navController = navController)}
+                            composable("Diary", enterTransition = {slideIntoContainer(
+                                towards = AnimatedContentTransitionScope.SlideDirection.Companion.Right,
+                                animationSpec = tween(700)
+                            )}, exitTransition = {fadeOut(animationSpec = tween(500))}) { diary(navController = navController)}
+
                         }
                     }
                 }
@@ -71,7 +106,7 @@ fun MyNavigationBar(navHostController: NavHostController) {
         mutableIntStateOf(3)
     }
 
-    val items = listOf("Calendar", "Diary", "ElSol")
+    val items = listOf("Calendar", "Diary")
 
     NavigationBar {
         items.forEachIndexed { index, item ->
@@ -80,8 +115,7 @@ fun MyNavigationBar(navHostController: NavHostController) {
                 icon = {
                     when (item) {
                         "Calendar" -> Icon(Icons.Filled.AccountBox, contentDescription = item)
-                        "Diary" -> Icon(Icons.Filled.Favorite, contentDescription = item)
-                        else -> Icon(Icons.Filled.Face, contentDescription = item)
+                        else -> Icon(Icons.Filled.Favorite, contentDescription = item)
                     }
                 },
                 onClick = {
@@ -89,11 +123,110 @@ fun MyNavigationBar(navHostController: NavHostController) {
                     when (selectedItem) {
                         0 -> navHostController.navigate("Calendar")
                         1 -> navHostController.navigate("Diary")
-                        2 -> navHostController.navigate("ElSol")
                     }
                 },
                 label = { Text(text = item) }
             )
         }
     }
+}
+
+sealed class DestinationScreen(val route: String) {
+    object SplashScreenDest : DestinationScreen(route =
+    "splash_screen")
+    object MainScreenDest : DestinationScreen(route = "Portada")
+}
+
+@Composable
+fun AnimationSplashContent(
+    scaleAnimation: Animatable<Float, AnimationVector1D>,
+    navController: NavController,
+    durationMillisAnimation: Int,
+    delayScreen: Long
+) {
+
+    LaunchedEffect(key1 = true) {
+        scaleAnimation.animateTo(
+            targetValue = 0.5F,
+            animationSpec = tween(
+                durationMillis = durationMillisAnimation,
+                easing = {
+                    OvershootInterpolator(3F).getInterpolation(it)
+                }
+            )
+        )
+
+        delay(timeMillis = delayScreen)
+
+        navController.navigate(route =
+        DestinationScreen.MainScreenDest.route) {
+            popUpTo(route =
+            DestinationScreen.SplashScreenDest.route) {
+                inclusive = true
+            }
+        }
+    }
+}
+
+@Composable
+fun DesignSplashScreen(
+    modifier: Modifier = Modifier,
+    imagePainter: Painter,
+    scaleAnimation: Animatable<Float, AnimationVector1D>
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(
+                        Color(239, 127, 26),
+                        Color(120, 40, 140),
+                        Color(0, 47, 187),
+                    )
+                )
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = imagePainter,
+                contentDescription = "Logotipo Splash Screen",
+                modifier = modifier
+                    .size(500.dp)
+                    .scale(scale = scaleAnimation.value),
+            )
+
+            Text(
+                text = "Remember everywhere",
+                color = Color.White,
+                fontSize = 40.sp,
+                fontWeight = FontWeight.ExtraBold,
+                fontFamily = FontTittle,
+                textAlign = TextAlign.Center,
+                modifier = modifier.scale(scale =
+                scaleAnimation.value)
+            )
+        }
+    }
+}
+
+@Composable
+fun SplashScreen(navController: NavController) {
+
+    val scaleAnimation: Animatable<Float, AnimationVector1D> =
+        remember { Animatable(initialValue = 0f) }
+
+    AnimationSplashContent(
+        scaleAnimation = scaleAnimation,
+        navController = navController,
+        durationMillisAnimation = 1500,
+        delayScreen = 3000L
+    )
+
+    DesignSplashScreen(
+        imagePainter = painterResource(id =
+        R.drawable.logomemominder),
+        scaleAnimation = scaleAnimation
+    )
 }
