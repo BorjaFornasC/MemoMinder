d0fbbd70e87cdccc1e116368809c6eea71a7801d
BorjaFornasC <bfornashiva@gmail.com>
2023-12-11 20:53:43 +0100

    Arreglo tema, cerquita de acabar

--- app/src/main/java/com/example/memominder/ui/theme/Theme.kt
+++ app/src/main/java/com/example/memominder/ui/theme/Theme.kt
@@ -12,17 +12,12 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = VibrantBlue,
+    secondary = VibrantYellow,
+    tertiary = GrayLight
 )
 
 private val LightColorScheme = lightColorScheme(
-    /*primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-     */
     primary = VibrantBlue,
     secondary = VibrantYellow,
     tertiary = GrayLight
